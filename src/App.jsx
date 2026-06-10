96bedd1b13923b863a7edd7a48ffa7b65ca964ec
shameer125 <alishameer251@gmail.com>
2026-06-09 21:03:47 -0700

    add toast notification

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,48 @@ import Services from "./components/Services";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-black text-white overflow-hidden">
-      {/* Background Effects */}
-      <div className="fixed inset-0 pointer-events-none z-0">
-        {/* Subtle radial gradient */}
-        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[800px] h-[800px] bg-red-500/5 rounded-full blur-[120px]" />
-        <div className="absolute bottom-0 right-0 w-[600px] h-[600px] bg-red-500/3 rounded-full blur-[100px]" />
-        {/* Grid overlay */}
-        <div className="absolute inset-0 bg-grid opacity-50" />
-      </div>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <div className="relative min-h-screen bg-black text-white overflow-hidden">
+        {/* Background Effects */}
+        <div className="fixed inset-0 pointer-events-none z-0">
+          {/* Subtle radial gradient */}
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[800px] h-[800px] bg-red-500/5 rounded-full blur-[120px]" />
+          <div className="absolute bottom-0 right-0 w-[600px] h-[600px] bg-red-500/3 rounded-full blur-[100px]" />
+          {/* Grid overlay */}
+          <div className="absolute inset-0 bg-grid opacity-50" />
+        </div>
 
-      {/* Content */}
-      <div className="relative z-10">
-        <Navbar />
-        <Hero />
-        <div className="section-divider" />
-        <About />
-        <div className="section-divider" />
-        <Skills />
-        <div className="section-divider" />
-        <Showreel />
-        <div className="section-divider" />
-        <Portfolio />
-        {/* <div className="section-divider" />
+        {/* Content */}
+        <div className="relative z-10">
+          <Navbar />
+          <Hero />
+          <div className="section-divider" />
+          <About />
+          <div className="section-divider" />
+          <Skills />
+          <div className="section-divider" />
+          <Showreel />
+          <div className="section-divider" />
+          <Portfolio />
+          {/* <div className="section-divider" />
         <BeforeAfter /> */}
-        <div className="section-divider" />
-        <Services />
-        {/* <div className="section-divider" />
+          <div className="section-divider" />
+          <Services />
+          {/* <div className="section-divider" />
         <Testimonials /> */}
-        <div className="section-divider" />
-        <Resume />
-        <div className="section-divider" />
-        <Contact />
-        <Footer />
+          <div className="section-divider" />
+          <Resume />
+          <div className="section-divider" />
+          <Contact />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
